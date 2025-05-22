a1c0ed515b1f25ef13cf1fa7818f69261edd32ff
Sri Sai Murari Makkala <srisaimurari10084@gmail.com>
2025-05-22 18:24:34 +0530

    odd or even

---
+++ practice/src/com/murari/oddOrEven.java
@@ -0,0 +1,16 @@
+package com.murari;
+
+import java.util.Scanner;
+//bitwise lessons
+public class oddOrEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        //odd or even
+        int odd = a & 1;
+        if(odd == 1)
+            System.out.println(a+" is odd");
+        else System.out.println(a+ " is even");
+
+    }
+}
