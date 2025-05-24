5812a16c9f29d368b2146dbfa018700f64ee229c
Sri Sai Murari Makkala <bunnymurari1@gmail.com>
2025-05-24 11:17:23 +0530

    magicnumber

--- practice/src/bytePrograms/nthMagicNumber.java
+++ practice/src/bytePrograms/nthMagicNumber.java
@@ -3,9 +3,9 @@ package bytePrograms;
 public class nthMagicNumber {
     public static void main(String[] args) {
         int n = 3;
-        int prod = 1,bit = 0,sum = 0, po = 5;
+        int prod = 1,sum = 0, po = 5;
         while(n!=0){
-            bit = n & 1;
+            int bit = n & 1;
             prod = (bit * po);
             sum = sum + prod;
             po = po*5;
