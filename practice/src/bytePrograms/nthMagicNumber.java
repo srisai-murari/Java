4f54c7ce3cce06e8b563175de6c1030f3e29c4ec
Sri Sai Murari Makkala <bunnymurari1@gmail.com>
2025-05-24 11:28:10 +0530

    magicnumber

--- practice/src/bytePrograms/nthMagicNumber.java
+++ practice/src/bytePrograms/nthMagicNumber.java
@@ -1,8 +1,15 @@
 package bytePrograms;
 
+import static java.lang.Math.log;
+import static java.lang.Math.pow;
+
 public class nthMagicNumber {
     public static void main(String[] args) {
-        int n = 3;
+        int n = 6;
+        //finding number of bits in a decimal number
+//total number of bits in binary logn(base2) = bitCount or n = 2 power bitcount
+        int logCount = (int)(log(n)/log(2))+1;
+        System.out.println(logCount);
         int prod = 1,sum = 0, po = 5;
         while(n!=0){
             int bit = n & 1;
