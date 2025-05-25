3072cb54f6f6789186c7c0580fbe66cd40fb576f
Sri Sai Murari Makkala <bunnymurari1@gmail.com>
2025-05-25 17:37:31 +0530

    25/5/25 roman to integers

---
+++ practice/src/LEETCODE/leetCode13.java
@@ -0,0 +1,86 @@
+package LEETCODE;
+
+import java.util.HashMap;
+
+public class leetCode13 {
+    public static void main(String[] args) {
+        String roman = "MCMXCIV";
+        int integer = FailedromanToInt(roman);
+        int integer2 = HashMapRomanToInt(roman);
+        System.out.println(integer2);
+    }
+
+    private static int HashMapRomanToInt(String roman) {
+        HashMap<Character, Integer> rom = new HashMap<>();
+        rom.put('I',1);
+        rom.put('V',5);
+        rom.put('X',10);
+        rom.put('L',50);
+        rom.put('C',100);
+        rom.put('D',500);
+        rom.put('M',1000);
+        int ans =0;//MCMXCIV
+        int an2 = rom.get(roman.charAt(0));
+        for (int i = 0; i < roman.length()-1; i++) {
+            int left = rom.get(roman.charAt(i));
+            int right = rom.get(roman.charAt(i+1));
+            if(left < right){
+                ans += (right - left);
+                i++;
+            }
+            else
+                ans += left;
+
+        }
+
+
+        return ans + rom.get(roman.charAt(roman.length()-1));
+
+    }
+
+    private static int FailedromanToInt(String str) {
+        char [] ch = str.toCharArray();
+        int ans = 0;
+        for (char c : ch) {
+            switch (c) {
+                case 'I':
+                    ans = ans + 1;
+                    break;
+                case 'V':
+                    ans = ans + 5;
+                    break;
+                case 'X':
+                    ans = ans + 10;
+                    break;
+                case 'L':
+                    ans = ans + 50;
+                    break;
+                case 'C':
+                    ans = ans + 100;
+                    break;
+                case 'D':
+                    ans = ans + 500;
+                    break;
+                case 'M':
+                    ans = ans + 1000;
+                    break;
+                default:
+
+            }
+        }
+        for (char c : ch) {
+            ans = switch (c) {
+                case 'I' -> ans + 1;
+                case 'V' -> ans + 5;
+                case 'X' -> ans + 10;
+                case 'L' -> ans + 50;
+                case 'C' -> ans + 100;
+                case 'D' -> ans + 500;
+                case 'M' -> ans + 1000;
+                default -> ans;
+            };
+        }
+
+        return ans;
+    }
+}
