404d21d880da92124fd52b0c7dcf2cb1ef6a0db1
Sri Sai Murari Makkala <bunnymurari1@gmail.com>
2025-05-25 17:44:28 +0530

    25/5/25
    updated roman to int

--- practice/src/LEETCODE/leetCode13.java
+++ practice/src/LEETCODE/leetCode13.java
@@ -4,11 +4,14 @@ import java.util.HashMap;
 
 public class leetCode13 {
     public static void main(String[] args) {
-        String roman = "MCMXCIV";
+        String roman = "III";
         int integer = FailedromanToInt(roman);
         int integer2 = HashMapRomanToInt(roman);
+//        int integer3 = ifelseRomanToInt(roman);
         System.out.println(integer2);
     }
+
+
 
     private static int HashMapRomanToInt(String roman) {
         HashMap<Character, Integer> rom = new HashMap<>();
@@ -20,13 +23,11 @@ public class leetCode13 {
         rom.put('D',500);
         rom.put('M',1000);
         int ans =0;//MCMXCIV
-        int an2 = rom.get(roman.charAt(0));
         for (int i = 0; i < roman.length()-1; i++) {
             int left = rom.get(roman.charAt(i));
             int right = rom.get(roman.charAt(i+1));
             if(left < right){
-                ans += (right - left);
-                i++;
+                ans -= left;
             }
             else
                 ans += left;
@@ -41,33 +42,7 @@ public class leetCode13 {
     private static int FailedromanToInt(String str) {
         char [] ch = str.toCharArray();
         int ans = 0;
-        for (char c : ch) {
-            switch (c) {
-                case 'I':
-                    ans = ans + 1;
-                    break;
-                case 'V':
-                    ans = ans + 5;
-                    break;
-                case 'X':
-                    ans = ans + 10;
-                    break;
-                case 'L':
-                    ans = ans + 50;
-                    break;
-                case 'C':
-                    ans = ans + 100;
-                    break;
-                case 'D':
-                    ans = ans + 500;
-                    break;
-                case 'M':
-                    ans = ans + 1000;
-                    break;
-                default:
 
-            }
-        }
         for (char c : ch) {
             ans = switch (c) {
                 case 'I' -> ans + 1;
